66abf2fbebdcf196f721fc3cd878c75ad4fc4feb
ike barranco <barranco.ike@outlook.com>
2020-05-01 06:42:40 -0600

    commented throughout app.js page

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ const render = require("./lib/htmlRenderer");
 
 const employees = [];
 
+/**
+ *      promptEmployeeQuestions()
+ * Purpose: prompt the user for basic employee information
+ * Parameters: None
+ * Return: obj of the answers given from the user
+ */
 function promptEmployeeQuestions()
 {
     return inquirer.prompt([
@@ -32,8 +38,14 @@ function promptEmployeeQuestions()
         }
     ]);
 
-}
+}//End promptEmployeeQuestions()
 
+/**
+ *      promptRole()
+ * Purpose: prompt the user to see what role the employee has
+ * Parameters: None
+ * Return: obj of the answer from the prompt choices
+ */
 function promptRole()
 {
     return inquirer.prompt([
@@ -44,8 +56,14 @@ function promptRole()
             choices: ["Engineer", "Intern", "Manager"]
         }
     ])
-}
+}//End promptRole()
 
+/**
+ *      promptEngineerQuestion()
+ * Purpose: prompt the user for the Engineers github username
+ * Parameters: None
+ * Return: obj of the answer from the prompt question
+ */
 function promptEngineerQuestion()
 {
     return inquirer.prompt([
@@ -55,8 +73,14 @@ function promptEngineerQuestion()
             message: "What is the employees github username?"
         },
     ]);
-}
+}//End promptEngineerQuestion()
 
+/**
+ *      promptInternQuestion()
+ * Purpose: prompt the user for the interns school name
+ * Parameters: None
+ * Return: obj of the answer from the prompt question
+ */
 function promptInternQuestion()
 {
     return inquirer.prompt([
@@ -66,8 +90,14 @@ function promptInternQuestion()
             message: "What school is the intern attending?"
         }    
     ]);
-}
+}//End promptInternQuestion()
 
+/**
+ *      promptVerifyInformation()
+ * Purpose: prompt the user for the managers office number
+ * Parameters: None
+ * Return: obj of the answer from the prompt question
+ */
 function promptManagerQuestion()
 {
     return inquirer.prompt([
@@ -77,8 +107,14 @@ function promptManagerQuestion()
             message: "What is the managers office number?"
         }
     ]);
-}
+}//End promptManagerQuestion()
 
+/**
+ *      promptVerifyInformation()
+ * Purpose: prompt the user if the information displayed is correct
+ * Parameters: None
+ * Return: True for the answer yes and False for the answer no
+ */
 function promptVerifyInformation()
 {
     return inquirer.prompt([
@@ -93,8 +129,14 @@ function promptVerifyInformation()
             return true;
         return false;
     });
-}
+}//end promptVerifyInformation()
 
+/**
+ *      promptForContinue()
+ * Purpose: prompt the user if they want to continue adding more employees
+ * Parameters: None
+ * Return: True for the answer yes and False for the answer no
+ */
 function promptForContinue()
 {
     return inquirer.prompt([
@@ -109,61 +151,87 @@ function promptForContinue()
             return true;
         return false;
     });
-}
+}//End promptForContinue()
 
-
+/**
+ *      init()
+ * Purpose: To initialize creation of the employees index.html using the prompt functions to ask the user questions
+ * Parameters: None
+ * Return: None
+ */
 async function init(){
-
+    //declare variable to be used for while loop
     let continueAdding = true;
 
     while(continueAdding === true)
     {
-        
+        //Create variable to store the Employee
         let newEmployee;
-        
+
+        //Prompt General Employee Answers
         let answers = await promptEmployeeQuestions();
+        //As what role the employee is
         let role = await promptRole();
 
+        //Check which role the employee is placed under
         switch(role.employeeRole)
         {
+            ///Employee is an engineer
             case "Engineer":
+                //Prompt user for the Engineer question and combine the answers
                 Object.assign(answers, await promptEngineerQuestion());
+                //Assign the new employee as an Engineer
                 newEmployee = new Engineer(...Object.values(answers));
                 break;
+            //Employee is an Intern
             case "Intern":
+                //Prompt user for the Engineer question and combine the answers
                 Object.assign(answers, await promptInternQuestion());
+                //Assign the new employee as an Intern
                 newEmployee = new Intern(...Object.values(answers));
                 break;
+                //Emplyee is an Manager
             case "Manager":
+                //Prompt user for the Engineer question and combine the answers
                 Object.assign(answers, await promptManagerQuestion());
+                //Assign the new employee as an Manager
                 newEmployee = new Manager(...Object.values(answers));
                 break;
             default:
                 break;
-        }
+        }//End Checking Employee Role
         
+        //Display Employee Information To The User
         console.log(newEmployee);
 
+        //Ask if the information generated is correct
         let correctInformation = await promptVerifyInformation();
+        //Information wasn't correct
         if(correctInformation === false)
-            continue;
+            continue;//Go back to the beginning of the while loop
 
+        //Add the employee in the employees array
         employees.push(newEmployee);
 
+        //Ask the user if they want to add another employee
         let addAnotherEmployee = await promptForContinue();
+        //User didnt want to add another employee
         if(addAnotherEmployee === false)
-            continueAdding = false;
+            continueAdding = false;//Make continue adding false
 
-    }    
-    console.log(employees);
+    }//End Employee Creation 
+    
+    //Create HTML using render 
     let htmlString = render(employees)
 
+    //Create index.html file
     fs.writeFile("index.html", htmlString, (err)=>{
         if(err)
             throw err;
+        //Tell the user that the file has been created
         console.log("Employee Files Have Been Created");
     });
 
-}
+}//end init()
 
 init();
