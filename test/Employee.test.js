98dbd47e514af829f10591540021c29bb8e6784b
ike barranco <barranco.ike@outlook.com>
2020-05-05 05:35:52 -0600

    Added initialization tests for Engineer test file

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,12 +1,13 @@
 const Employee = require("../lib/Employee");
 
-test("Can instantiate Employee instance", () => {
-  const e = new Employee();
-  expect(typeof(e)).toBe("object");
-});
 
 describe("Initializing the Employee Class", () =>{
 
+  test("Can instantiate Employee instance", () => {
+    const e = new Employee();
+    expect(typeof(e)).toBe("object");
+  });
+  
   test("Can set name via constructor arguments", () => {
     const name = "Alice";
     const e = new Employee(name);
--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,4 +1,48 @@
 const Engineer = require("../lib/Engineer");
+describe("Initialization of Engineer class", () =>{
+
+  test("Can instantiate Employee instance", () => {
+    const e = new Engineer();
+    expect(typeof(e)).toBe("object");
+  });
+
+  test("Can set name via Contructor Arguments", () =>{
+    const name = "todd";
+    const e = new Engineer(name);
+    expect(e.name).toBe(name);
+  });
+
+  test("Can set ID via Constructor Arguments", () =>{
+    const name = "todd";
+    const id = 1;
+
+    const e = new Engineer(name, id);
+
+    expect(e.id).toBe(id);
+  });
+
+  test("Can set email via Contructor Arguments", () =>{
+    const name = "todd";
+    const id = 1;
+    const email = "example@email.com";
+
+    const e = new Engineer(name, id, email);
+    
+    expect(e.email).toBe(email);
+  });
+
+  test("Can set github username via Constructor Arguments", () => {
+    const name = "todd";
+    const id = 1;
+    const email = "example@email.com";
+    const gitHub = "example";
+
+    const e = new Engineer(name, id, email, gitHub);
+
+    expect(e.github).toBe(gitHub)
+  });
+
+});
 
 describe("Test Engineer Class Methods", () =>{
 
@@ -19,6 +63,6 @@ describe("Test Engineer Class Methods", () =>{
     const e = new Engineer("Foo", 1, "test@test.com", testValue);
     expect(e.getGithub()).toBe(testValue);
   });
-  
+
 });
 
